9c44d396ee9da141896e06479a3f6299ab5ee896
lukezedutchman <33630598+lukezedutchman@users.noreply.github.com>
2020-02-21 13:09:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <?php
+
+    include 'rows/head.php';
+
+    ?>
+
+    <title>Bubbletalks.com</title>
+</head>
+<body>
+
+<div class="body_overlap">
+    <?php
+
+    include 'rows/row_header.php';
+
+    require 'rows/row_main_content.php';
+
+    include 'rows/row_footer.php';
+
+    ?>
+</div>
+
+</body>
+</html>
